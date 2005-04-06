427b04ab89a0f8f21eb5d258e4991d695e0d194b
arodland <arodland@344b0559-0ef4-0310-9719-c7061cca2820>
2005-04-06 06:44:19 +0000

    add license header for convert.pl

--- trunk/convert.pl
+++ trunk/convert.pl
@@ -1,3 +1,13 @@
+###
+# WikiOnCD Conversion Tool
+# Part of WikiOnCD
+# Copyright (C) 2005, Andrew Rodland <arodland@entermail.net>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+
 use IO::File;
 require 'bzr.pm';
 
