5fa843e0de5c3e0efb0bd3714f699a6bb040efe0
arodland <arodland@344b0559-0ef4-0310-9719-c7061cca2820>
2005-04-07 08:35:06 +0000

    Complete rewrite of Compress::Bzip2::RandomAccess, to use a single-file format. Hasn't been tested yet.

--- trunk/wikioncd/bzr.pm
+++ trunk/wikioncd/bzr.pm
@@ -20,34 +20,33 @@ sub new_to_file {
 
 	my $package = ref($class) || $class || __PACKAGE__;
 
-	open my $datafh, '>:raw', "$file.bz2" or return undef;
-	open my $indexfh, '>:raw', "$file.index" or return undef;
+	open my $fh, '>:raw', $file or return undef;
 
 	our $default_blocksize;
 	
 	$blocksize = $blocksize || $default_blocksize;
 
-	print $indexfh pack("N", 0xc001d00d), pack("N", $blocksize);
+	print $fh pack("N", 0xc001d00d), pack("N", $blocksize);
 
 	$blocksize *= 1024;
 	
 	my $self = {
-		data => $datafh,
-		'index' => $indexfh,
+		fh => $datafh,
 		blocksize => $blocksize,
 	};
 
 	bless $self, $package;
 }
 
-sub write_file {
-	my ($self, $filename, $data) = @_;
+sub file {
+	my ($self, $filename, $len) = @_;
 
-	my $len = length $data;
+	print { $self->{fh} } pack("N", $len);
+	print { $self->{fh} } "$filename\n";
+}
 
-	print { $self->{index} } pack("N", $len);	
-	print { $self->{index} } "$filename\n";
-	
+sub data {
+	my ($self, $data) = @_;
 	$self->{buffer} .= $data;
 
 	while (length($self->{buffer}) >= $self->{blocksize}) {
@@ -55,19 +54,30 @@ sub write_file {
 	}
 }
 
+
+sub write_file {
+	my ($self, $filename, $data) = @_;
+
+	my $len = length $data;
+
+	$self->file($filename, $len);
+	$self->data($data);
+}
+
 sub write_file_from_file {
 	my ($self, $key, $filename) = @_;
 
 	my $data;
 	
-	{
-		local $/;
-		open my $fh, '<:raw', $filename;
-		$data = <$fh>;
-		close $fh;
-	}
+	open my $fh, '<:raw', $filename;
+	my $size = -s $fh;
+	
+	$self->file($key, $size);
 
-	return $self->write_file($key, $data);
+	while (read $fh, $data, $self->{blocksize}) {
+		$self->data($data);
+	}
+	close $fh;
 }
 
 
@@ -79,16 +89,21 @@ sub flush {
 	my $compressed = Compress::Bzip2::compress($buf);
 
 	my $len = length $compressed;
-	print { $self->{index} } pack("N", $len | 0x80000000);
-	print { $self->{data} } $compressed;
+	
+	if ($len >= 0x80000000) {
+		die "Trying to write a block of 2^31 bytes or more. Sorry.";
+	}
+	
+	print { $self->{fh} } pack("N", $len | 0x80000000);
+	print { $self->{fh} } $compressed;
+	
 }
 
 sub close_for_write {
 	my ($self) = @_;
 	
 	$self->flush;
-	close $self->{index};
-	close $self->{data};
+	close $self->{fh};
 }
 
 sub new_from_file {
@@ -96,11 +111,10 @@ sub new_from_file {
 
 	my $package = ref($class) || $class || __PACKAGE__;
 
-	open my $datafh, '<:raw', "$file.bz2" or return undef;
-	open my $indexfh, '<:raw', "$file.index" or return undef; 
+	open my $fh, '<:raw', $file or return undef;
 
-	read $indexfh, my $magic, 4;
-	read $indexfh, my $blocksize, 4;
+	read $fh, my $magic, 4;
+	read $fh, my $blocksize, 4;
 	
 	$blocksize = unpack("N", $blocksize);
 	
@@ -111,83 +125,135 @@ sub new_from_file {
 	}
 
 	my $self = {
-		data => $datafh,
-		'index' => $indexfh,
+		fh => $indexfh,
 		blocksize => $blocksize,
 	};
 
 	bless $self, $package;
 
-	$self->do_index;
 	return $self;
 
 }
 
-sub do_index {
-	my ($self) = @_;
+sub cache_offsets {
+	my $self = shift;
 
-	my $fpos = 0;
-	my $cpos = 0;
-	my $block = 0;
+	my $file_pos = 0; my $block_pos = 4; my $block = 0;
 
-	while (!eof($self->{'index'})) {
-		read $self->{'index'}, my $code, 4;
-		my $len = unpack("N", $code);
+	while (!eof($self->{fh})) { # Won't likely trigger, but...
+
+		seek $self->{fh}, $block_pos, SEEK_SET;
 		
+		read $self->{fh}, my $code, 4;
+		last if eof($self->{fh});
+		my $len = unpack("N", $code);
+
 		if ($len & 0x80000000) {
 			$len &= 0x7fffffff;
-			$self->{block}[$block] = [ $cpos, $len ];
-			$cpos += $len;
+			$self->{block}[$block] = [ $block_pos, $len ];
+			$block_pos += 4 + $len;
 			$block ++;
 		} else {
-			chomp(my $filename = readline $self->{'index'});
-			$self->{files}{$filename} = [$fpos, $len];
-#			push @{$self->{filelist}}, [$x, $fpos, $y];
-			$fpos += $len;
+			my $filename = readline $self->{fh};
+			$block_pos += 4 + length $filename;
+			chomp($filename);
+
+			$self->{files}{$filename} = [ $file_pos, $len ];
+			$file_pos += $len;
+		}
+	}
+}	
+
+sub find_file_cached {
+	my ($self, $filename) = @_;
+
+	my ($fpos, $flen) = @{ $self->{files}{$file} };
+	
+	my $block = int($fpos / $self->{blocksize});
+	my $skip = $fpos % $self->{blocksize};
+	
+	my ($bpos, $blen) = @{ $self->{block}{$block} };
+
+	seek $self->{fh}, $bpos, SEEK_SET;
+
+	return ($flen, $skip);
+}
+
+sub find_file_nocache {
+	my ($self, $wantfile) = @_;
+	
+	my $file_pos = 0; my $block_pos = 4; my $block = 0;
+	my ($want_pos, $want_len);
+	
+	while (!eof($self->{fh})) {
+
+		seek $self->{fh}, $block_pos, SEEK_SET;
+		read $self->{fh}, my $code, 4 or die $!;
+		my $len = unpack("N", $code);
+
+		if ($len & 0x80000000) {
+			if (defined $want_pos && ($block + 1) * $blocksize >= $want_pos) {
+				seek $self->{fh}, $block_pos, SEEK_SET; #Back it up a bit.
+				return ($want_len, $want_pos % self->{blocksize});
+			}
+				
+			$len &= 0x7fffffff;
+			$block_pos += 4 + $len;
+			$block ++;
+		} else {
+			my $filename = readline $self->{fh};
+			$block_pos += 4 + length $filename;
+			chomp($filename);
+
+			if ($filename eq $wantfile) {
+				$want_pos = $file_pos;
+			}
+			$file_pos += $len;
 		}
 	}
 }
 
-sub get_block {
-	my ($self, $block) = @_;
-#	print STDERR "B: $block ";
-	my ($offset, $len) = @{ $self->{block}[$block] };
-#	print "@ $offset:$len\n";
+sub decompress_one_block {
+	my $self = shift;
+	
+	my $code;
+	read $self->{fh}, $code, 4;
+	my $len = unpack("N", $code);
+	
+	die "Doesn't look like a block" unless $len & 0x80000000;
+	$len &= 0x7fffffff;
+
 	my $data;
-
-	seek $self->{data}, $offset, SEEK_SET;
-	read $self->{data}, $data, $len;
-
-	$data = Compress::Bzip2::decompress($data);
-	return $data;
+	read $self->{fh}, $data, $len or die $!;
+	return Compress::Bzip2::decompress($data);
 }
 
 sub read_file {
 	my ($self, $file) = @_;
 
-#	print STDERR "$file\n";
-	my ($offset, $len) = @{ $self->{files}{$file} };
-
-	my $block = int($offset / $self->{blocksize});
-	my $skip = $offset % $self->{blocksize};
-
-	my $data = $self->get_block($block);
-	substr($data, 0, $skip) = undef;
-
-	while (!eof($self->{data}) && length $data < $len) {
-		$block++;
-		$data .= $self->get_block($block);
+	my ($len, $skip);
+	
+	if (defined $self->{block} && defined $self->{files}) {
+		($len, $skip) = $self->find_file_cached($file);
+	} else {
+		($len, $skip) = $self->find_file_nocache($file);
 	}
 
-	substr($data, $len) = undef; #Trim excess
-		return $data;
+	my $data = $self->decompress_one_block();
+	substr ($data, 0, $skip) = undef;
+	
+	while (length $data < $len) {
+		$data .= $self->decompress_one_block();
+	}
+
+	substr($data, $len) = undef;
+	return $data;
 }
 
 sub close_for_read {
 	my ($self) = @_;
 
-	close $self->{'index'};
-	close $self->{data};
+	close $self->{fh};
 }
 
 1;
