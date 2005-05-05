3ec128472f743b52eb6cb0e45a9c041da719e85f
arodland <arodland@344b0559-0ef4-0310-9719-c7061cca2820>
2005-05-05 04:41:34 +0000

    Tables seem to be working

--- trunk/wikioncd/parser.pl
+++ trunk/wikioncd/parser.pl
@@ -144,30 +144,58 @@ sub do_template {
 	print join ',', map { "''$_''" } @params;
 	print ")<br />";
 }
-	
+
+{
+	my %closemap =
+		(em1 => 'em1_close',
+		 em2 => 'em2_close',
+		 em3 => 'em3_close',
+		 caption => 'table_caption_close',
+		 row => 'table_row_close',
+		 col => 'table_cell_close',
+		 header => 'table_header_close',
+		 table => 'table_close',
+		);
+
+	sub close_tags {
+		my $st = shift;
+		my $cb = shift;
+
+		for my $thing (@_) {
+			if ($st->{$thing}) {
+				$cb->{$closemap{$thing}}->();
+				$st->{$thing} --;
+			}
+		}
+	}
+
+}
+
 sub parse_wiki {
 	my ($data, $cb) = @_;
 
-	my $list = "";
-	my $pre = 0;
+	my %st;
+	@st{qw(list)} = qw();
 
 	while ($data !~ /\G\z/cg) {
 		if ($data =~ /\G\n\n/cgs) {
-			do_pre(\$data, \$pre, $cb);
-			if ($data =~ /\G(?<=\n)([*#]+)/cg) {
-				do_list($1, \$list, $cb);
+			do_pre(\$data, \$st{pre}, $cb);
+			if ($data =~ /\G(?:(?<=\n)|\A)([*#]+)/cg) {
+				do_list($1, \$st{list}, $cb);
 			} else {
-				do_list("", \$list, $cb);
+				do_list("", \$st{list}, $cb);
 			}
 			$cb->{paragraph}->();
+			close_tags(\%st, $cb, qw(em1 em2 em3 caption));
 		} elsif ($data =~ /\G\n/cgs) {
-			do_pre(\$data, \$pre, $cb);
-			if ($data =~ /\G(?<=\n)([*#]+)/cg) {
-				do_list($1, \$list, $cb);
+			do_pre(\$data, \$st{pre}, $cb);
+			if ($data =~ /\G(?:(?<=\n)|\A)([*#]+)/cg) {
+				do_list($1, \$st{list}, $cb);
 			} else {
-				do_list("", \$list, $cb);
+				do_list("", \$st{list}, $cb);
 			}
 			$cb->{whitespace}->();
+			close_tags(\%st, $cb, qw(em1 em2 em3 caption));
 		} elsif ($data =~ /\G\s+/cg) {
 			$cb->{whitespace}->();
 		} elsif ($data =~ m#\G<nowiki>(.*?)</nowiki>#cgis) {
@@ -176,8 +204,72 @@ sub parse_wiki {
 			$cb->{math}->($1);
 		} elsif ($data =~ /\G<!--(.*?)-->/cgs) {
 			$cb->{comment}->($1);
-		} elsif ($data =~ /\G(?<=\n)-----*/cg) {
+		} elsif ($data =~ /\G(?:(?<=\n)|\A)-----*/cg) {
 			$cb->{divider}->();
+		} elsif ($data =~ /\G(?:(?<=\n)|\A){\|(.*)/cg) {
+			$cb->{table_open}->($1);
+			$st{row} = 0;
+			$st{table} ++;
+		} elsif ($data =~ /\G(?:(?<=\n)|\A)\|\}/cg) {
+			close_tags(\%st, $cb, qw(col header row table));
+		} elsif ($data =~ /\G(?:(?<=\n)|\A)\|-(.*?)/cg) {
+			close_tags(\%st, $cb, qw(col header row));
+			$cb->{table_row_open}->($1);
+			$st{row} = 1;
+		} elsif ($data =~ /\G(?:(?<=\n)|\A)\|\+/cg) {
+			$cb->{table_caption_open}->();
+			$st{caption} = 1;
+		} elsif ($data =~ /\G((?:(?<=\n)|\A)\||\|\|)/cg) {
+			my $start = pos($data);
+			my ($link, $bar) = (undef, undef);
+			my $params;
+
+			if ($data =~ /\G.*?\[\[/cg) {
+				$link = pos($data);
+			}
+			pos($data) = $start;
+			if ($data =~ /\G.*?\|/cg) {
+				$bar = pos($data);
+			}
+
+			if (defined($bar) && (!defined($link) || $bar < $link)) {
+				$params = substr($data, $start, ($bar - $start - 1));
+				pos($data) = $bar;
+			} else {
+				$params = "";
+				pos($data) = $start;
+			}
+
+			close_tags(\%st, $cb, qw(col header));
+
+			$st{row} || $cb->{table_row_open}->("");
+			$cb->{table_cell_open}->($params);
+			@st{qw(row col)} = qw(1 1);
+		} elsif ($data =~ /\G((?:(?<=\n)|\A)!|!!)/cg) {
+			my $start = pos($data);
+			my ($link, $bar) = (undef, undef);
+			my $params;
+
+			if ($data =~ /\G.*\[\[/cg) {
+				$link = pos($data);
+			}
+			pos($data) = $start;
+			if ($data =~ /\G.*\|/cg) {
+				$bar = pos($data);
+			}
+
+			if (defined($bar) && (!defined($link) || $bar < $link)) {
+				$params = substr($data, $start, ($bar - $start - 1));
+				pos($data) = $bar;
+			} else {
+				$params = "";
+				pos($data) = $start;
+			}
+
+			close_tags(\%st, $cb, qw(col header));
+			$st{row} || $cb->{table_row_open}->("");
+			$cb->{table_header_open}->($params);
+			@st{qw(row header)} = qw(1 1);
 		} elsif ($data =~ /\G\[\[/cg) {
 			pos($data) -= 2;
 			my $link = match_balanced(\$data, "[[", "]]");
@@ -193,26 +285,26 @@ sub parse_wiki {
 			substr($template, -2, 2, '');
 			do_template($template);
 		} elsif ($data =~ /\G''''/cg) {
-			my $start = pos($data);
-			$data =~ m#\G.*?''#cg;
-			my $len = pos($data) - $start - 4;
-			$cb->{em3_open}->();
-			parse_wiki(substr($data, $start, $len), $cb);
-			$cb->{em3_close}->();
+			if ($st{em3}) {
+				close_tags(\%st, $cb, 'em3');
+			} else {
+				$cb->{em3_open}->();
+				$st{em3} = 1;
+			}
 		} elsif ($data =~ /\G'''/cg) {
-			my $start = pos($data);
-			$data =~ m#\G.*?'''#cg;
-			my $len = pos($data) - $start - 3;
-			$cb->{em2_open}->();
-			parse_wiki(substr($data, $start, $len), $cb);
-			$cb->{em2_close}->();
+			if ($st{em2}) {
+				close_tags(\%st, $cb, 'em2');
+			} else {
+				$cb->{em2_open}->();
+				$st{em2} = 1;
+			}
 		} elsif ($data =~ /\G''/cg) {
-			my $start = pos($data);
-			$data =~ m#\G.*?''#cg;
-			my $len = pos($data) - $start - 2;
-			$cb->{em1_open}->();
-			parse_wiki(substr($data, $start, $len), $cb);
-			$cb->{em1_close}->();
+			if ($st{em1}) {
+				close_tags(\%st, $cb, 'em1');
+			} else {
+				$cb->{em1_open}->();
+				$st{em1} = 1;
+			}
 		} elsif ($data =~ /\G====(.*?)====/cg) {
 			$cb->{sec3}->($1);
 		} elsif ($data =~ /\G===(.*?)===/cg) {
@@ -223,47 +315,55 @@ sub parse_wiki {
 			$data =~ /\G(.[[:alnum:] <>\/]*)/cgi;
 			$cb->{text}->($1);
 		}
-	}
-}
+		}
+		}
 
-die "WTF" unless @ARGV == 1;
-open my $in, '<', $ARGV[0] or die $!;
-my $data = do { local $/; <$in>; };
-close $in;
+		die "WTF" unless @ARGV == 1;
+		open my $in, '<', $ARGV[0] or die $!;
+		my $data = do { local $/; <$in>; };
+		close $in;
 
-print "<html><body>";
-parse_wiki($data, {
-		'text' => sub { print $_[0] },
-		'nowiki' => sub { print $_[0] },
-		'paragraph' => sub { print "<p />\n\n" },
-		'list' => sub { print "<br />List: <code>$_[0]</code>\n"; },
-		'link' => sub { print "<code>$_[0]</code>" },
-		'template' => sub { print "<code>$_[0]</code>" },
-		'em1_open' => sub { print "<i>"; },
-		'em1_close' => sub { print "</i>"; },
-		'em2_open' => sub { print "<b>"; },
-		'em2_close' => sub { print "</b>"; },
-		'em3_open' => sub { print "<b><i>"; },
-		'em3_close' => sub { print "</i></b>"; },
-		'sec1' => sub { print "<h1>$_[0]</h1>\n" },
-		'sec2' => sub { print "<h2>$_[0]</h2>\n" },
-		'sec3' => sub { print "<h3>$_[0]</h3>\n" },
+		print "<html><body>";
+		parse_wiki($data, {
+				'text' => sub { print $_[0] },
+				'nowiki' => sub { print $_[0] },
+				'paragraph' => sub { print "<p />\n\n" },
+				'list' => sub { print "<br />List: <code>$_[0]</code>\n"; },
+				'link' => sub { print "<code>$_[0]</code>" },
+				'template' => sub { print "<code>$_[0]</code>" },
+				'em1_open' => sub { print "<i>"; },
+				'em1_close' => sub { print "</i>"; },
+				'em2_open' => sub { print "<b>"; },
+				'em2_close' => sub { print "</b>"; },
+				'em3_open' => sub { print "<b><i>"; },
+				'em3_close' => sub { print "</i></b>"; },
+				'sec1' => sub { print "<h1>$_[0]</h1>\n" },
+				'sec2' => sub { print "<h2>$_[0]</h2>\n" },
+				'sec3' => sub { print "<h3>$_[0]</h3>\n" },
 #		'comment' => sub { print "<!-- $_[0] -->" },
-		'comment' => sub { 1 },
-		'divider' => sub { print "<hr>\n" },
-		'whitespace' => sub { print " "; },
-		'list_#_open' => sub { print "<ol>" },
-		'list_#_item_open' => sub { print "<li>" },
-		'list_#_item_close' => sub { print "</li>" },
-		'list_#_close' => sub { print "</ol>" },
-		'list_*_open' => sub { print "<ul>" },
-		'list_*_item_open' => sub { print "<li>" },
-		'list_*_item_close' => sub { print "</li>" },
-		'list_*_close' => sub { print "</ul>" },
-		'pre_open' => sub { print "<pre>" },
-		'pre_close' => sub { print "</pre>" },
-		'link' => sub { print qq(<a href="/$_[0]">$_[1]</a>); },
+				'comment' => sub { 1 },
+				'divider' => sub { print "<hr>\n" },
+				'whitespace' => sub { print " "; },
+				'list_#_open' => sub { print "<ol>" },
+				'list_#_item_open' => sub { print "<li>" },
+				'list_#_item_close' => sub { print "</li>" },
+				'list_#_close' => sub { print "</ol>" },
+				'list_*_open' => sub { print "<ul>" },
+				'list_*_item_open' => sub { print "<li>" },
+				'list_*_item_close' => sub { print "</li>" },
+				'list_*_close' => sub { print "</ul>" },
+				'pre_open' => sub { print "<pre>" },
+				'pre_close' => sub { print "</pre>" },
+				'link' => sub { print qq(<a href="/$_[0]">$_[1]</a>); },
+				'table_open' => sub { print qq(<table $_[0]>\n) },
+				'table_close' => sub { print "</table>\n" },
+				'table_row_open' => sub { print qq(<tr $_[0]>\n) },
+				'table_row_close' => sub { print "</tr>\n" },
+				'table_cell_open' => sub { print qq(<td $_[0]>\n) },
+				'table_cell_close' => sub { print "</td>\n" },
+				'table_caption_open' => sub { print "<caption>\n" },
+				'table_caption_close' => sub { print "</caption>\n" },
 		});
 
-print "</body></html>";
+		print "</body></html>";
 
